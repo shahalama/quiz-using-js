836f7796010f3fc206c8f055bfad8a09cab315aa
SHAH ALAM <69628088+shahalama@users.noreply.github.com>
2021-01-24 16:11:20 +0530

    Add files via upload

---
+++ quiz_question.js
@@ -0,0 +1,55 @@
+
+
+const quiz = [
+	{
+		q:'What is software?',
+		options:['Instructions that tell the hardware what to do','Any part of the computer that has a physical structure','Clothing designed to be worn by computer users','Flexible parts of a computer case'],
+		answer:0
+	},
+	{
+		q:'Which of the following are types of computer hardware? Select all that apply.',
+		options:['Firefox','Chrome','RAM','Microsoft Windows'],
+		answer:2
+	},
+	{
+		q:'The computer’s main circuit board is called a ________.',
+		options:['monitor','hard drive','Bluetooth card','motherboard'],
+		answer:3
+	},
+	{
+		q:'Which feature is being used in this image?',
+		options:['USB port','Wi-Fi','Touchpad','Webcam'],
+		answer:2
+	},
+	{
+		q:'RAM is like a computer’s ________, while a hard drive is like a computer’s ________.',
+		options:['long-term memory/short-term memory','brain/nervous system','nervous system/brain','short-term memory/long-term memory'],
+		answer:3
+	},
+	{
+		q:'What is an Ethernet port used for?',
+		options:['Providing power to the computer','Creating new user accounts','Connecting smartphones and other peripherals','Connecting to the Internet'],
+		answer:3
+	},
+	{
+		q:'What is Wi-Fi?',
+		options:['A type of software that scans for viruses','A type of sound card','An extra-wide computer case used by servers','A type of wireless network'],
+		answer:3
+	},
+	{
+		q:'Windows, macOS, and Linux are examples of ________.',
+		options:['mobile devices','operating system','web browsers','Internet Service provider'],
+		answer:1
+	},
+	{
+		q:'What is the full form of "FORTRAN"?',
+		options:['Foreign Translation','Forex Transfer','Formula Translation','Formatting and Translating'],
+		answer:2
+	},
+	{
+		q:'What does "GUI" stand for?',
+		options:['Gateway using Intel','Graphical user interface','Global user index','Good used iPhone'],
+		answer:1
+	}
+
+]
